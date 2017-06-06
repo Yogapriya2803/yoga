65294f9ed7a457fa8bec88f5bd982f07f0f446c6
Yogapriya2803 <yoga.priyaece@ksrce.ac.in>
2017-06-06 15:51:46 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+printf("it is positive number");
+else if(n<0)
+printf("it is negative number");
+else
+printf("it is zero");
+}

e1f76326d5239f51a4211adf0a292ed4fe74db40
Yogapriya2803 <yoga.priyaece@ksrce.ac.in>
2017-07-14 10:09:17 +0530

    Add files via upload

---
+++ IntegerEqualsIndex.java
@@ -0,0 +1,16 @@
+package guviprgm;
+import java.util.*;
+public class IntegerEqualsIndex {
+ public static void main(String []args){
+	        Scanner s=new Scanner(System.in);
+	        int n=s.nextInt();
+	        int ar[]=new int[n];
+	        for(int i=0;i<n;i++)
+	            ar[i]=s.nextInt();
+	        for(int i=0;i<n;i++){
+	            if(ar[i]==i)
+	                System.out.println(ar[i]);
+	        }
+	     }
+	}
+

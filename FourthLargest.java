7cc5ba576bcb7cbfe5953a283d641a50ac21956d
Yogapriya2803 <yoga.priyaece@ksrce.ac.in>
2017-07-17 14:22:50 +0530

    Add files via upload

---
+++ FourthLargest.java
@@ -0,0 +1,28 @@
+package guvi;
+import java.util.*;
+public class FourthLargest {
+	public static void main(String ar[])throws Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int n,t;
+	n=sc.nextInt();
+	int a[]=new int[n];
+	for(int i=0;i<n;i++)
+	a[i]=sc.nextInt();
+	for(int i=0;i<n-1;i++)
+	{
+	for(int j=i+1;j<n;j++)
+	{
+	if(a[i]<a[j])
+	{
+	t=a[i];
+	a[i]=a[j];
+	a[j]=t;
+	}
+	}
+	}
+	System.out.println("fourth tallest student:"+a[3]);
+	}
+
+	}
+

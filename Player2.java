30b3d045d8055080201a2250b9f92793a405d4a9
Yogapriya2803 <yoga.priyaece@ksrce.ac.in>
2017-07-17 09:52:59 +0530

    Add files via upload

---
+++ Player2.java
@@ -0,0 +1,27 @@
+package src;
+import java.util.Scanner;
+public class Player2 {
+
+		@SuppressWarnings("resource")
+		public static void main(String[] args) {
+
+	int input=new Scanner(System.in).nextInt();
+	int values=0;
+	while(input>=10){
+		values=0;
+		while(input!=0){
+			int temp=input%10;
+			values=values+(temp*temp);
+			input=input/10;
+		}
+		input=values;
+	}
+	if(input==1){
+		System.out.println("happy");
+	}
+	else
+		System.out.println("not happy");
+		}
+
+	}
+
